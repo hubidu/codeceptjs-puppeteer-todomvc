690ab8daece1a4f4fb3b0988a9f8f557ca193f9a
Eduard Lind <eduard.lind@check24.de>
2019-02-25 15:46:42 +0100

    Add alternative to assert

---
+++ todo-mvc-app/step_04_A/pages/todos.page.js
@@ -0,0 +1,91 @@
+const assert = require('assert')
+
+const I = actor();
+
+const nthTodoCheckbox = nth => ({ xpath: `(//*[contains(@class,"todo-list")]/li/div/input)[${nth}]`})
+const nthTodoItem = nth => ({ xpath: `(//*[contains(@class,"todo-list")]/li)[${nth}]`})
+const nthXpathTodoItemNotCompleted = nth => ({ xpath: `(//*[contains(@class,"todo-list")])/li[${nth}][not(@class = "completed")]`})
+const nthXpathTodoItemCompleted = nth => ({ xpath: `(//*[contains(@class,"todo-list")])/li[${nth}][@class = "completed"]`})
+
+module.exports = new class TodoPage {
+    goto() {
+        I.amOnPage('http://todomvc.com/examples/angularjs/#/')
+        I.refreshPage()
+        I.waitForVisible('.new-todo')    
+    }
+
+    enterTodo(todo) {
+        I.fillField('.new-todo', todo)
+        I.pressKey('Enter')        
+    }
+
+    async markNthAsCompleted(nthTodo) {
+        const classNames = await I.grabAttributeFrom(nthTodoItem(nthTodo), 'class')
+        assert(classNames.indexOf('completed') < 0, 'Expected todo to be not already marked as completed')
+        I.click(nthTodoCheckbox(nthTodo))
+    }
+
+    async unmarkNthAsCompleted(nthTodo) {
+        const classNames = await I.grabAttributeFrom(nthTodoItem(nthTodo), 'class')
+        assert(classNames.indexOf('completed') >= 0, 'Expected todo to be marked as completed')
+        I.click(nthTodoCheckbox(nthTodo))
+    }
+
+    markAllAsCompleted() {
+        I.click('label[for="toggle-all"')
+    }
+
+    clearCompleted() {
+        I.click('button.clear-completed')
+    }
+
+    filterAll() {
+        I.click(locate('.filters li').at(1))
+    }
+
+    filterActive() {
+        I.click(locate('.filters li').at(2))
+    }
+
+    filterCompleted() {
+        I.click(locate('.filters li').at(3))
+    }
+
+    async seeNthTodoEquals(nthTodo, todo) {
+        let todos = await I.grabTextFrom('.todo-list li')
+        if (typeof todos === 'string') {
+            todos = [todos]
+        }
+
+        assert(todos[nthTodo - 1] === todo, `Expected "${todo}" but got "${todos[nthTodo - 1]}"`)
+        return todos
+    }
+
+    seeNumberOfTodos(numberOfTodos) {
+        I.seeNumberOfVisibleElements('.todo-list li', numberOfTodos)
+    }
+    
+    seeEmptyTodoInput() {
+        I.seeInField('.new-todo', '')
+    }
+
+    seeFooter() {
+        I.seeElement('footer.info')
+    }
+
+    // Xpath alternative
+
+    markNthXpathAsCompleted(nthTodo) {
+        I.waitForVisible(nthXpathTodoItemNotCompleted(nthTodo))
+        I.click(nthXpathTodoItem(nthTodo))
+    }
+
+    unmarkNthXpathAsCompleted(nthTodo) {
+        I.waitForVisible(nthXpathTodoItemCompleted(nthTodo))
+        I.click(nthXpathTodoItem(nthTodo))
+    }
+
+    seeNthXpathTodoEquals(nthTodo, todo) {
+        I.waitForVisible(`//*[contains(@class,"todo-list")]/li[${nthTodo}][descendant::label[contains(text(), "${todo}")]]`)
+    }
+}
